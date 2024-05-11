4c73c425ed18014b333e26ee11cd2ffb77f1eb2b
promoteAI <1035959050@qq.com>
2024-05-11 23:26:21 +0800

    python 动态规划

--- src/data_structure_and_algorithm/dynamic_programming/climb_stairs/02_climb_stairs_dfs_mem.py
+++ src/data_structure_and_algorithm/dynamic_programming/climb_stairs/02_climb_stairs_dfs_mem.py
@@ -4,7 +4,7 @@
 # @Author   :Hanjiang Chen
 # @Email    :chen_han_jiang@163.com
 # @Project  :structure_and_algorithm 
-# @File     :02_climb_stairs_dfs.py
+# @File     :02_climb_stairs_dfs_mem.py
 # @Software :PyCharm
 
 """
@@ -15,20 +15,26 @@ from typing import List
 
 
 class Solution:
-    def dfs(self, n: int):
+    def dfs(self, n: int, mem: List[int]):
         if n == 1 or n == 2:
             return n
+        if mem[n] != 0:
+            return mem[n]
         # 状态转移方程 # dp[i] = dp[i-1] + dp[i-2]
-        cnt = self.dfs(n - 1) + self.dfs(n - 2)
+        cnt = self.dfs(n - 1, mem) + self.dfs(n - 2, mem)
+        # 记录dp[i]
+        mem[n] = cnt
         return cnt
 
     def climbStairs(self, n: int) -> int:
-        cnt = self.dfs(n)
+        # 引入额外存储，避免重复计算重叠子问题
+        mem = [0] * (n + 1)
+        cnt = self.dfs(n, mem)
         return cnt
 
 
 if __name__ == '__main__':
-    # 法二:暴力搜索:问题分解的角度分析
+    # 法三:记忆化搜素,重复子问题只计算一次
     n = 3
     res = Solution().climbStairs(n)
     print(f"爬{n}阶楼梯总的方案数量:{res}")
